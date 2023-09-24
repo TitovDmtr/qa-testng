c2bada8955e7bb177db00ed392a4ab29e47b5515
dmytro <dmytro.titov@airslate.com>
2023-09-24 15:59:24 -0400

    Simple tests fix

--- src/test/java/ua/ithillel/testng/SeleniumTest.java
+++ src/test/java/ua/ithillel/testng/SeleniumTest.java
@@ -1,16 +1,11 @@
 package ua.ithillel.testng;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
-import java.util.List;
 
 public class SeleniumTest {
     @Test
@@ -19,10 +14,7 @@ public class SeleniumTest {
 
         driver.manage().window().maximize(); // open full size window
         driver.get("https://www.google.com/?hl=en-US");
-
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        //wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[normalize-space()='Accept All']"))).click();
-
         Assert.assertEquals(driver.getTitle(), "Google", "Strings aren't matched");
 
         driver.quit();
